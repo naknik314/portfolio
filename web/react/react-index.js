1a4d7f19d69648e0572400eedd5730d8697633cd
artedecimo <10149210+artedecimo@users.noreply.github.com>
2021-06-18 19:54:43 -0400

    Delete web/react directory

--- web/react/react-index.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom';
-import './react.css';
-import ReactObject from './reactobject';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<ReactObject />, document.getElementById('root'));
-registerServiceWorker();
